0e1690c4212dfce9c9494fac5b0d604c409866dc
Chanuka Gunadasa <Chanuka.Gunadasa@IRIS.CO.UK>
2024-08-24 22:44:23 +0530

    JwtAuthGuard applies to Create Rating API

--- src/ratings/ratings.controller.ts
+++ src/ratings/ratings.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { RatingsService } from './ratings.service';
 import { CreateRatingDto } from './dto/create-rating.dto';
 import { UpdateRatingDto } from './dto/update-rating.dto';
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
 
 @Controller('ratings')
 export class RatingsController {
   constructor(private readonly ratingsService: RatingsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createRatingDto: CreateRatingDto) {
     return this.ratingsService.create(createRatingDto);
